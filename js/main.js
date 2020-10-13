7200ec972be9b6de9df2885d53e61108a4f20842
Nicola Invernizzi <nginvernizzi1@gmail.com>
2020-10-13 15:20:55 +0200

    exercize done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What's your name?");
+
+var lastName = prompt("What's your last name?");
+
+var color = prompt("What's your favourite color?");
+
+console.log(firstName, lastName, color, "19")
+
+document.getElementById("pwd").innerHTML = firstName + lastName + color + 19
